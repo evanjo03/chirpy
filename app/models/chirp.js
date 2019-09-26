d8dffbc27f1677f80fb4a04fd9f042fd4590d49f
John Evans <john.wes.evans@gmail.com>
2019-09-26 11:20:25 -0500

    testing

--- app/models/chirp.js
+++ app/models/chirp.js
@@ -1,20 +1,8 @@
-// Dependencies
-// =============================================================
-
-// This may be confusing but here Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
-
-// Creates a "Chirp" model that matches up with DB
-var Chirp = sequelize.define("chirp", {
-  author: Sequelize.STRING,
-  body: Sequelize.STRING,
-  created_at: Sequelize.DATE
-});
-
-// Syncs with DB
-Chirp.sync();
-
-// Makes the Chirp Model available for other files (will also create a table)
-module.exports = Chirp;
+module.exports = function (sequelize, DataTypes) {
+  var Chirp = sequelize.define("Chirp", {
+    author: DataTypes.STRING,
+    body: DataTypes.STRING,
+    created_at: DataTypes.DATE
+  });
+  return Chirp;
+};
